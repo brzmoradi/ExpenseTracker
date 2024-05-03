06b2a3c4cc70ad78424857f784be366f7ef18610
Behrooz <b.moradi@emofid.com>
2024-05-04 02:10:23 +0330

    - add authorize with bearer token in swagger UI

--- src/main/java/com/behrooz/expensetracker/config/swagger/SwaggerConfig.java
+++ src/main/java/com/behrooz/expensetracker/config/swagger/SwaggerConfig.java
@@ -1,9 +1,12 @@
 package com.behrooz.expensetracker.config.swagger;
 
 import com.behrooz.expensetracker.config.ApplicationProperties;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +23,18 @@ public class SwaggerConfig {
 
   @Bean
   public OpenAPI springShopOpenAPI() {
+    final String securitySchemeName = "Token";
     return new OpenAPI()
+        .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
         .info(
             new Info()
                 .title(groupName)
